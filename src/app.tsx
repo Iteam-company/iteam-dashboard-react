0693c6a4a97af22169e8f68830da74e9a9f8d5fa
Misha Kravtsov <aniahucm@gmail.com>
2023-02-11 10:49:55 +0200

    fix case of app component naming

--- src/app.tsx
+++ src/app.tsx
@@ -1,11 +1,11 @@
 import { ThemeProvider } from '@mui/material';
-import React, { memo, useMemo, FC } from 'react';
+import React, { memo, useMemo, FC, PropsWithChildren } from 'react';
 import { useRoutes } from 'react-router-dom';
 import { useAppSelector } from './hooks/store/use-app-selector-hook';
 import { selectRoutesSchemaByUserRole } from './store/slices/auth-slice';
 import { themes } from './themes';
 
-export const App: FC<object> = memo(() => {
+export const App: FC<PropsWithChildren> = memo(() => {
 	// in the future we get theme from redux store and make it extendable
 	const currentTheme = useMemo(() => themes[0], [themes]);
 	const routesByRole = useAppSelector(selectRoutesSchemaByUserRole);
