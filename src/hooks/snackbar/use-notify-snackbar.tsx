e0318283b963ae0001c44566ed34f04cc830bcd5
Maxim Gumeniuk <gumeniukmaks0510@gmail.com>
2023-02-13 15:55:47 +0200

    make custom close button for snackbar

--- src/hooks/snackbar/use-notify-snackbar.tsx
+++ src/hooks/snackbar/use-notify-snackbar.tsx
@@ -1,15 +1,23 @@
 import React, { useCallback } from 'react';
 import { useSnackbar, VariantType } from 'notistack';
+import { Box } from '@mui/material';
 
 export const useNotifySnackbar = () => {
 	const { enqueueSnackbar, closeSnackbar } = useSnackbar();
 	const action = (snackbarId: string | number) => (
-		<button
+		<Box
+			sx={{
+				width: '30px',
+				height: '30px',
+				cursor: 'pointer',
+				position: 'absolute',
+				left: '5%'
+			}}
 			onClick={() => {
 				closeSnackbar(snackbarId);
 			}}>
-			close
-		</button>
+
+		</Box >
 	);
 
 	const showSnackbar = useCallback(
