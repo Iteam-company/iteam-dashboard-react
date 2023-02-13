560959c5a147a7f741aa4d8f7173e4f90bdb516a
Maxim Gumeniuk <gumeniukmaks0510@gmail.com>
2023-02-13 16:15:53 +0200

    prettier fixes

--- src/types/api/auth/sign-in.dto.ts
+++ src/types/api/auth/sign-in.dto.ts
@@ -1,5 +1,5 @@
-import { User } from "../../common/api/user";
-import { Tokens } from "../../common/api/user/user-tokens";
+import { User } from '../../common/api/user';
+import { Tokens } from '../../common/api/user/user-tokens';
 
 export interface SignInDto {
 	tokens: Tokens;
