3e8198da95076d0591512dc74718458aec19833a
Maxim Gumeniuk <gumeniukmaks0510@gmail.com>
2023-02-14 12:40:38 +0200

    import snackbar in index.tsx was deleted

--- src/index.tsx
+++ src/index.tsx
@@ -6,14 +6,15 @@ import { BrowserRouter } from 'react-router-dom';
 import { HelmetProvider } from 'react-helmet-async';
 import { Provider } from 'react-redux';
 import { persistor, store } from './store';
-import { SnackbarKey, SnackbarProvider } from 'notistack';
+import { PersistGate } from 'redux-persist/integration/react';
+import { SnackbarProvider } from 'notistack';
 
 // importing fonts
 import '@fontsource/roboto/300.css';
 import '@fontsource/roboto/400.css';
 import '@fontsource/roboto/500.css';
 import '@fontsource/roboto/700.css';
-import { PersistGate } from 'redux-persist/integration/react';
+
 const container = document.getElementById('root') as HTMLElement;
 const root = createRoot(container);
 root.render(
