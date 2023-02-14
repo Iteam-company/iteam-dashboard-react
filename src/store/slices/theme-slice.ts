a9952d6ed7f9fd56628b374640bd8463efef316a
Maxim Gumeniuk <gumeniukmaks0510@gmail.com>
2023-02-14 17:34:47 +0200

    logic and functionality for switching color themes

---
+++ src/store/slices/theme-slice.ts
@@ -0,0 +1,21 @@
+import { createSlice } from "@reduxjs/toolkit";
+import { RootState } from "..";
+import { ThemeSliceStoreType } from "../../types/store/slices/theme-slice";
+
+const initialState: ThemeSliceStoreType = {
+	isDark: false,
+};
+
+const ThemeSlice = createSlice({
+name: 'theme',
+initialState,
+reducers: {
+  setDarkTheme(state) {
+    state.isDark = !state.isDark;
+  },
+}
+});
+
+export const ThemeReducer = ThemeSlice.reducer;
+export const { setDarkTheme } = ThemeSlice.actions;
+export const darkTheme = (state: RootState) => state.theme.isDark
--- src/themes/index.ts
+++ src/themes/index.ts
@@ -1,5 +1,15 @@
 import { createTheme, Theme } from '@mui/material';
+import { ColorMode } from '../constants/theme/theme-color';
 
-export const themes: Array<Partial<Theme> | ((outerTheme: Theme) => Theme)> = [
-	createTheme(),
-];
+export const themes: { [key in ColorMode]: Theme} = {
+	[ColorMode.DARK]: createTheme({
+		palette: {
+			mode: ColorMode.DARK,
+		},
+	}),
+	[ColorMode.LIGHT]: createTheme({
+		palette: {
+			mode: ColorMode.LIGHT,
+		},
+	}),
+}
