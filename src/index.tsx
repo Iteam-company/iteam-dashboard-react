3d08c3deddabdc91ee16f42e6a826f2009d0624d
Maxim Gumeniuk <gumeniukmaks0510@gmail.com>
2023-02-14 21:40:46 +0200

    some lempty line was added

--- src/index.tsx
+++ src/index.tsx
@@ -8,7 +8,6 @@ import { Provider } from 'react-redux';
 import { persistor, store } from './store';
 import { PersistGate } from 'redux-persist/integration/react';
 import { SnackbarProvider } from 'notistack';
-import { CssVarsProvider } from '@mui/joy/styles';
 
 // importing fonts
 import '@fontsource/roboto/300.css';
