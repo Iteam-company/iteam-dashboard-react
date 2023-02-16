54aaf87bdebaf4ef86fcefdb90f24995089f6449
Maxim Gumeniuk <gumeniukmaks0510@gmail.com>
2023-02-16 15:24:29 +0200

    add switch for color Mode and change logic in color theme slice and change usecolortheme hook

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,16 @@
 import { ThemeProvider } from '@mui/material';
-import React, { memo, useMemo, FC, PropsWithChildren } from 'react';
+import React, { memo, FC, PropsWithChildren } from 'react';
 import { useRoutes } from 'react-router-dom';
-import { ColorMode } from './constants/theme/theme-color';
-import { useThemeDetector } from './hooks/color-theme/use-colot-theme-detector';
+import { useColorTheme } from './hooks/color-theme/use-color-theme-detector';
 import { useAppSelector } from './hooks/store/use-app-selector-hook';
 import { selectRoutesSchemaByUserRole } from './store/slices/auth-slice';
-import { darkTheme } from './store/slices/theme-slice';
-import { themes } from './themes';
-
-
 
 export const App: FC<PropsWithChildren> = memo(() => {
-	const DarkTheme = useAppSelector(darkTheme);
-	const isDarkTheme = useThemeDetector();
+	const currentTheme = useColorTheme();
 	// in the future we get theme from redux store and make it extendable
-	const currentTheme = useMemo(() => DarkTheme || isDarkTheme ? themes[ColorMode.DARK] : themes[ColorMode.LIGHT], [themes, DarkTheme, isDarkTheme]);
 	const routesByRole = useAppSelector(selectRoutesSchemaByUserRole);
 	// console.log(routesByRole, 'routesByRoleroutesByRole');
 	const content = useRoutes(routesByRole);
-
 	return <ThemeProvider theme={currentTheme}>{content}</ThemeProvider>;
 });
 
