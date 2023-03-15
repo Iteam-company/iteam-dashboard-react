cdff66aab15c1cfef959f0b7ecadbd71c0bcfd03
Maxim Gumeniuk <gumeniukmaks0510@gmail.com>
2023-03-15 14:30:31 +0200

    before change branch

--- src/ui/components/common/button-delete/index.tsx
+++ src/ui/components/common/button-delete/index.tsx
@@ -5,7 +5,9 @@ import { DeleteUserModal } from '../delete-user-modal';
 
 export const ButtonDelete = () => {
 	const [open, setOpen] = useState(false);
-	const handleOpen = () => setOpen(true);
+	const handleOpen = () => {
+		setOpen(true);
+	};
 	const handleClose = () => setOpen(false);
 	return (
 		<>
