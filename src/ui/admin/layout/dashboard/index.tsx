8fddfdd76cc9bdaca1f3ddb20ba443ea27b98014
Misha Kravtsov <aniahucm@gmail.com>
2023-03-28 18:50:29 +0300

    change favicon + removed unused png

--- src/ui/admin/layout/dashboard/index.tsx
+++ src/ui/admin/layout/dashboard/index.tsx
@@ -1,6 +1,5 @@
 import { Box } from '@mui/material';
 import { memo, useCallback, useState } from 'react';
-import { ViewDefaultPage } from '../../../components/common/view-default-page';
 import { AppBar } from '../../components/layout/dashboard/app-bar';
 import { AdminSidebar } from '../../components/layout/dashboard/side-bar';
 import { ViewContainer } from '../../components/layout/dashboard/view-container';
