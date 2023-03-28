0ab0be240167a2e186a695805429535b8d18212e
Misha Kravtsov <aniahucm@gmail.com>
2023-03-28 20:02:13 +0300

    implemented useHelmetHook

--- src/app.tsx
+++ src/app.tsx
@@ -1,18 +1,29 @@
 import { ThemeProvider } from '@mui/material';
 import { memo, FC, PropsWithChildren } from 'react';
 import { useRoutes } from 'react-router-dom';
+import { useHelmet } from './hooks/helmet/use-helmet';
 
 import { useAppSelector } from './hooks/store/use-app-selector-hook';
 import { selectRoutesSchemaByUserRole } from './store/slices/auth-slice';
 import { selectTheme } from './store/slices/theme-slice';
 
 export const App: FC<PropsWithChildren> = memo(() => {
+	// get theme
 	const theme = useAppSelector(selectTheme);
 
-	// in the future we get theme from redux store and make it extendable
+	// get mock up depend of role of user
 	const routesByRole = useAppSelector(selectRoutesSchemaByUserRole);
 	const content = useRoutes(routesByRole);
-	return <ThemeProvider theme={theme}>{content}</ThemeProvider>;
+
+	// get tab title within helmet in hook
+	const { helmet } = useHelmet();
+
+	return (
+		<ThemeProvider theme={theme}>
+			{helmet}
+			{content}
+		</ThemeProvider>
+	);
 });
 
 App.displayName = 'App';
